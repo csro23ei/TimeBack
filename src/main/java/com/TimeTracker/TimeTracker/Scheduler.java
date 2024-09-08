c275b9098e2138baefba633fa58de745ef65cceb
csro23ei <dante030724@gmail.com>
2024-09-08 11:27:16 +0200

    something

--- src/main/java/com/TimeTracker/TimeTracker/Scheduler.java
+++ src/main/java/com/TimeTracker/TimeTracker/Scheduler.java
@@ -1,9 +1,5 @@
 package com.TimeTracker.TimeTracker;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
 @Component
 @EnableScheduling
 public class Scheduler {
